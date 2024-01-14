08a79891b2668181021fe897851f234e3a78adc9
AmgaadShokrallah <amgaad@example.com>
2024-01-13 20:18:26 -0800

    commit

---
+++ tests/test_console.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""console.py."""
+import os
+import sys
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from models import storage
+from models.engine.file_storage import FileStorage
+from console import HBNBCommand
+
+
+class TestHBNBCommand_prompting(unittest.TestCase):
+    """testing prompting of the HBNB command interpreter."""
+    def test_prompt_string(self):
+        self.assertEqual("(hbnb) ", HBNBCommand.prompt)
+
+    def test_empty_line(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd(""))
+            self.assertEqual("", output.getvalue().strip())
+
+
+class TestHBNBCommand_help(unittest.TestCase):
+    """testing help messages of the HBNB command interpreter."""
+
+    def test_help_quit(self):
+        h = "Quit : to exit the program."
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help quit"))
+            self.assertEqual(h, output.getvalue().strip())
+
+    def test_help_EOF(self):
+        h = "EOF signal : to exit the program."
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertFalse(HBNBCommand().onecmd("help EOF"))
+            self.assertEqual(h, output.getvalue().strip())
+
+
+class TestHBNBCommand_exit(unittest.TestCase):
+    """testing exiting from the HBNB command interpreter."""
+
+    def test_quit_exits(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(HBNBCommand().onecmd("quit"))
+
+    def test_EOF_exits(self):
+        with patch("sys.stdout", new=StringIO()) as output:
+            self.assertTrue(HBNBCommand().onecmd("EOF"))
